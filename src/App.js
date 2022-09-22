66610f4f1ece40fec7800d20bd82fc89e34cf848
doyindev1 <ade.doyin27@gmail.com>
2022-09-22 21:42:31 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import './App.css';
+import './App.css'
+import Home from './components/Home';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
 
 function App() {
   return (
-    <div className="App">
-      Portfolio 2.0
-    </div>
+    <>
+   <Router>
+    <Routes>
+      <Route exact path="/" element={<Home />}/>
+    </Routes>
+   </Router>
+    
+    </>
   );
 }
 
