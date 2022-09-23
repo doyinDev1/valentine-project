6bfbe49b9c9b65c1c937b0fb711d76a69d64966b
doyindev1 <ade.doyin27@gmail.com>
2022-09-23 23:08:43 +0100

    login page done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css'
-import Home from './components/Home';
+import Home from './components/Home/Home';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Landing from './components/Landing/Landing';
 
 
 function App() {
@@ -9,6 +10,8 @@ function App() {
    <Router>
     <Routes>
       <Route exact path="/" element={<Home />}/>
+      <Route exact path="/lover" element={<Landing />}/>
+
     </Routes>
    </Router>
     
